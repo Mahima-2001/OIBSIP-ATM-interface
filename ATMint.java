445f49420607ffa04b00ce5d644b78925e1799a1
CHAUDHARI MAHIMA SURYAKANT <99661594+Mahima-2001@users.noreply.github.com>
2022-08-31 18:38:34 +0530

    Add files via upload

---
+++ ATMint.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class ATMint extends OptionMenu {
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionMenu = new OptionMenu();
+
+		optionMenu.getLogin();
+	}
+}
